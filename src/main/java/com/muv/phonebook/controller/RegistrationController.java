0b48d069519d02976ec8e12fe2e352c6769b2086
muv11 <uliamozgovaya03@gmail.com>
2023-01-12 14:01:38 +0500

    Create controller for registration, add get mapping and post mapping that post data required for registration

---
+++ src/main/java/com/muv/phonebook/controller/RegistrationController.java
@@ -0,0 +1,37 @@
+package com.muv.phonebook.controller;
+
+import com.muv.phonebook.model.User;
+import com.muv.phonebook.service.RegistrationService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class RegistrationController {
+
+    private final RegistrationService registrationService;
+
+    public RegistrationController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+
+    @GetMapping("/registration")
+    public String getRegistration() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String postRegistration(
+            @RequestParam String login,
+            @RequestParam String password,
+            @RequestParam String email
+    ) {
+        User user = new User(login, password, email);
+        if (registrationService.registerUser(user)) {
+            return "authorization";
+        }
+        return "errors/registrationError";
+    }
+
+}
