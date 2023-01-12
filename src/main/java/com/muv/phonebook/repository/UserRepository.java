e72574225424269fb021d871552950892963ed83
muv11 <uliamozgovaya03@gmail.com>
2023-01-12 13:57:42 +0500

    Create CRUD interface for User

---
+++ src/main/java/com/muv/phonebook/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.muv.phonebook.repository;
+
+import com.muv.phonebook.model.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository {
+    //CRUD
+    //create
+    boolean createUser(User user);
+    //read
+    User getUserById(Long id);
+    //update
+    boolean updateUser(User user);
+    //delete
+    boolean deleteUser(User user);
+    //getAll
+    List<User> getAll();
+}
