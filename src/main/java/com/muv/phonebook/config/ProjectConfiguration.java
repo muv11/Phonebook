c23e079b0444fd509b38f38dadf4c8ae008decab
muv11 <uliamozgovaya03@gmail.com>
2023-01-12 13:54:00 +0500

    Create project configuration, set data source

---
+++ src/main/java/com/muv/phonebook/config/ProjectConfiguration.java
@@ -0,0 +1,34 @@
+package com.muv.phonebook.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+
+@Configuration
+@ComponentScan(basePackages = "java")
+@PropertySource("classpath:application.properties")
+public class ProjectConfiguration {
+
+    Environment environment;
+
+    public ProjectConfiguration(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource managerDataSource = new DriverManagerDataSource();
+        managerDataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("spring.datasource.driver-class-name")));
+        managerDataSource.setUrl(Objects.requireNonNull(environment.getProperty("spring.datasource.url")));
+        managerDataSource.setUsername(Objects.requireNonNull(environment.getProperty("spring.datasource.username")));
+        managerDataSource.setPassword(Objects.requireNonNull(environment.getProperty("spring.datasource.password")));
+        return managerDataSource;
+    }
+
+}
