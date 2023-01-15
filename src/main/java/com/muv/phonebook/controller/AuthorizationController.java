d5e9bdf1ed3e68eefe2139e1627d9f016ca50ebe
muv11 <uliamozgovaya03@gmail.com>
2023-01-15 13:16:44 +0500

    Add post mapping method that posts user's login and password

--- src/main/java/com/muv/phonebook/controller/AuthorizationController.java
+++ src/main/java/com/muv/phonebook/controller/AuthorizationController.java
@@ -1,10 +1,19 @@
 package com.muv.phonebook.controller;
 
+import com.muv.phonebook.service.AuthorizationService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizationController {
+
+    private final AuthorizationService authorizationService;
+
+    public AuthorizationController(AuthorizationService authorizationService) {
+        this.authorizationService = authorizationService;
+    }
 
     @GetMapping("/")
     public String getAuthorization() {
@@ -16,5 +25,15 @@ public class AuthorizationController {
         return "redirect:registration";
     }
 
+    @PostMapping("/")
+    public String postAuthorization(
+            @RequestParam String login,
+            @RequestParam String password
+    ) {
+        if (authorizationService.isUserCorrect(login, password)) {
+            return "phonebook";
+        }
+        return "errors/authorizationError";
+    }
 
 }
