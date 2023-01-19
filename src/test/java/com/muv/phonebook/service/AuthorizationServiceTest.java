297021118f967cd591afe80b3d5db9d3a03b6610
muv11 <uliamozgovaya03@gmail.com>
2023-01-19 14:15:48 +0500

    Create tests for authorization service

---
+++ src/test/java/com/muv/phonebook/service/AuthorizationServiceTest.java
@@ -0,0 +1,21 @@
+package com.muv.phonebook.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServiceTest {
+
+    @Autowired
+    private AuthorizationService authorizationService;
+
+    @Test
+    void isUserCorrect() {
+        Assertions.assertTrue(authorizationService.isUserCorrect("newsitelogin", "Pass12345"));
+        Assertions.assertFalse(authorizationService.isUserCorrect("newsitelogin", "passworD5464"));
+    }
+
+
+}
