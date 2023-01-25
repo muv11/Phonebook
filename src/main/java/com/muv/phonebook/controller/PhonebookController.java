cdd2e50a93dc404b80a1c9bb6dde816662f5f7ba
muv11 <uvmozgovaya@proton.me>
2023-01-25 13:25:02 +0500

    Add get mapping for phonebook page

--- src/main/java/com/muv/phonebook/controller/PhonebookController.java
+++ src/main/java/com/muv/phonebook/controller/PhonebookController.java
@@ -1,10 +1,24 @@
 package com.muv.phonebook.controller;
 
+import com.muv.phonebook.service.PhonebookService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PhonebookController {
+
+    private final PhonebookService phonebookService;
+
+    public PhonebookController(PhonebookService phonebookService) {
+        this.phonebookService = phonebookService;
+    }
+
+    @GetMapping("/phonebook")
+    public String getPhonebook(Model model) {
+
+        return "phonebook";
+    }
 
     @GetMapping("/redirectToSettings")
     public String redirectSettings() {
