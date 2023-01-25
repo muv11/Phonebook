a4412a8f5f6a425ddf924d2458471fc6d5cc3f75
muv11 <uliamozgovaya03@gmail.com>
2023-01-25 13:26:41 +0500

    Create RowMapper for Contact model

---
+++ src/main/java/com/muv/phonebook/model/ContactMapper.java
@@ -0,0 +1,24 @@
+package com.muv.phonebook.model;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ContactMapper implements RowMapper<Contact> {
+    @Override
+    public Contact mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Contact contact = new Contact();
+        contact.setId(rs.getLong("id"));
+        contact.setLastName(rs.getString("last_name"));
+        contact.setName(rs.getString("name"));
+        contact.setFathersName(rs.getString("fathers_name"));
+        contact.setPhoneNumber(rs.getString("phone_number"));
+        contact.setCity(rs.getString("city"));
+        contact.setStreet(rs.getString("street"));
+        contact.setFlatNumber(rs.getString("flat_number"));
+        contact.setEmail(rs.getString("email"));
+        contact.setIdUser(rs.getLong("id_user"));
+        return contact;
+    }
+}
