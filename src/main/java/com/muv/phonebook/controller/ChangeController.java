0baacb523c74e8b8c81e6a7e73c5e3f9c830250a
muv11 <uliamozgovaya03@gmail.com>
2023-01-25 13:27:14 +0500

    Add get mappings for pages

---
+++ src/main/java/com/muv/phonebook/controller/ChangeController.java
@@ -0,0 +1,14 @@
+package com.muv.phonebook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChangeController {
+
+    @GetMapping("/change")
+    public String getAdd() {
+        return "change";
+    }
+
+}
