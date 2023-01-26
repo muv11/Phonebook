45c3693482cce575b16895e2b9379b48d7ddab81
muv11 <uliamozgovaya03@gmail.com>
2023-01-26 12:18:37 +0500

    Add method that finds user's id by its login

--- src/main/java/com/muv/phonebook/service/PhonebookService.java
+++ src/main/java/com/muv/phonebook/service/PhonebookService.java
@@ -1,24 +1,29 @@
 package com.muv.phonebook.service;
 
 import com.muv.phonebook.model.Contact;
-import com.muv.phonebook.model.User;
 import com.muv.phonebook.repository.ContactRepository;
+import com.muv.phonebook.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PhonebookService {
 
     private final ContactRepository contactRepository;
+    private final UserRepository userRepository;
 
-    public PhonebookService(ContactRepository contactRepository) {
+    public PhonebookService(ContactRepository contactRepository, UserRepository userRepository) {
         this.contactRepository = contactRepository;
+        this.userRepository = userRepository;
     }
 
-    public List<Contact> getContactList(User user) {
-        return contactRepository.findAllByUserId(user.getId());
+    private Long getUserIdByLogin(String login) {
+        return userRepository.findUserByLogin(login).getId();
+    }
+
+    public List<Contact> getContactList(String login) {
+        return contactRepository.findAllByUserId(getUserIdByLogin(login));
     }
 
 }
