ea892992ce83c7e5699e06fc0ce5aa7fc4411086
muv11 <uvmozgovaya@proton.me>
2023-01-26 12:17:40 +0500

    Add output of the contacts' table to the get mapping

--- src/main/java/com/muv/phonebook/controller/PhonebookController.java
+++ src/main/java/com/muv/phonebook/controller/PhonebookController.java
@@ -1,22 +1,32 @@
 package com.muv.phonebook.controller;
 
+import com.muv.phonebook.model.Contact;
+import com.muv.phonebook.service.LoggedUserManagementService;
 import com.muv.phonebook.service.PhonebookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class PhonebookController {
 
     private final PhonebookService phonebookService;
+    private final LoggedUserManagementService userManagementService;
 
-    public PhonebookController(PhonebookService phonebookService) {
+    public PhonebookController(PhonebookService phonebookService, LoggedUserManagementService userManagementService) {
         this.phonebookService = phonebookService;
+        this.userManagementService = userManagementService;
     }
 
     @GetMapping("/phonebook")
     public String getPhonebook(Model model) {
-
+        if (userManagementService.getLogin() == null) {
+            return "authorization";
+        }
+        List<Contact> contacts = phonebookService.getContactList(userManagementService.getLogin());
+        model.addAttribute("contactsList", contacts);
         return "phonebook";
     }
 
